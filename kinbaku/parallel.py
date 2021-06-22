5c22cef8cd8e63f4b6f35ebddbd44171e19e1303
kerighan <max.chbx@gmail.com>
2021-06-22 17:45:58 +0200

    from parallelism to threading - experimental

--- kinbaku/parallel.py
+++ kinbaku/parallel.py
@@ -1,47 +1,45 @@
+import threading, queue
+
+
 def parallel_task(G, nodes, task, n_jobs=-1):
-    import multiprocessing as mp
+    q = queue.Queue()
 
     if n_jobs == -1:
-        n_jobs = mp.cpu_count()
+        import multiprocessing as mp
+        n_jobs = mp.cpu_count() * 5
 
-    with mp.Manager() as manager:
-        q = manager.Queue()
-        d = manager.dict()
-        for node in nodes:
-            q.put(node)
-
-        processes = []
-        for _ in range(n_jobs):
-            q.put(None)
-            if task == "neighbors":
-                p = mp.Process(target=target_neighbors,
-                               args=(q, d, G), daemon=True)
-            elif task == "predecessors":
-                p = mp.Process(target=target_predecessors,
-                               args=(q, d, G), daemon=True)
-            else:
-                raise ValueError(f"Unknown task {task}")
-            p.start()
-            processes.append(p)
-
-        for p in processes:
-            p.join()
-
-        data = dict(d)
-    return data
+    d = {}
+    for _ in range(n_jobs):
+        if task == "neighbors":
+            thread = threading.Thread(
+                target=target_neighbors, args=(q, d, G))
+        else:
+            thread = threading.Thread(
+                target=target_predecessors, args=(q, d, G))
+        thread.daemon = True
+        thread.start()
+    
+    for node in nodes:
+        q.put_nowait(node)
+    q.join()
+    return {}
 
 
 def target_neighbors(q, d, G):
     while True:
-        item = q.get()
-        if item is None:
+        try:
+            item = q.get()
+        except queue.Empty:
             return
         d[item] = list(G.neighbors(item))
+        q.task_done()
 
 
 def target_predecessors(q, d, G):
     while True:
-        item = q.get()
-        if item is None:
+        try:
+            item = q.get()
+        except queue.Empty:
             return
         d[item] = list(G.predecessors(item))
+        q.task_done()
